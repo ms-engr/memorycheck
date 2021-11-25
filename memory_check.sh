4d7f0a777f08bab81b1ed68592ed52b064bf31b0
ms-engr <monica.mamitag@gmail.com>
2021-11-25 16:11:04 +0800

    3

--- memory_check.sh
+++ memory_check.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
+trap "exit 1" term
+export TOP_PID=$$
 
 function memorycheck(){
     memusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f%\n"), $3/$2*100}' |  awk '{print $3}' | cut -d"." -f1)
     echo "Memory usage is $memusage%"
      }
-     
-     
-function check(){
-    
-}     
-function verify(){
-    if  [ "$input1" -gt "$input2"  ]
-    then  
-      check
-    else 
-      memorycheck
-    fi
-}
-
-        
-function help(){
-    echo "Please provide the following parameters"
-}
-
-
     
 function parameter(){ while getopts ":cwe:" opt; do
    case $opt in
@@ -48,6 +30,44 @@ function parameter(){ while getopts ":cwe:" opt; do
       verify
         fi
 }
+
+function verify(){
+    if  [ "$input1" -gt "$input2"  ]
+    then  
+      check
+    else 
+      parameter
+    fi
+}
+
+        
+function help(){
+    echo "Please provide the following parameters"
+    parameter
+}
+
+function check(){
+    if [ "$memusage" -ge "$input1" ]
+    then 
+    SUBJECT=" `date '+%Y%m%d %H:%M'` memory check - critical "
+    MESSAGE="/tmp/Mail.out"
+    TO="monica.mamitag@gmail.com"
+  echo "Current memory is: $memusage%" >> $MESSAGE
+  echo "" >> $MESSAGE
+  echo "+------------------------------------------------------------------+" >> $MESSAGE
+  echo "Top 10 processes consuming high memory:" >> $MESSAGE
+  echo "+------------------------------------------------------------------+" >> $MESSAGE
+  echo "$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -10)" >> $MESSAGE
+  mail -s "$SUBJECT" "$TO" < $MESSAGE
+  rm /tmp/Mail.out
+    elif [ "$memusage" -ge "$input2" ]
+    then
+     kill -s TERM $TOP_PID
+    elif ["$memusage" -lt "$input2"]
+    then
+    kill -s TERM $TOP_PID
+    fi
+}     
 
 memorycheck
 parameter
