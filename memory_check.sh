f20c2e3ad9f0152c6aff7951506cc0c8f2d0239d
ms-engr <monica.mamitag@gmail.com>
2021-11-25 15:09:09 +0800

    2

--- memory_check.sh
+++ memory_check.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
-function memorycheck () {
-    memusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f%\n"), $3/$2*100}' |  awk '{print $3}' | cut -d"." -f1)}
+
+function memorycheck(){
+    memusage=$(free | awk '/Mem/{printf("RAM Usage: %.2f%\n"), $3/$2*100}' |  awk '{print $3}' | cut -d"." -f1)
+    echo "Memory usage is $memusage%"
+     }
+     
+     
+function check(){
+    
+}     
+function verify(){
+    if  [ "$input1" -gt "$input2"  ]
+    then  
+      check
+    else 
+      memorycheck
+    fi
+}
+
+        
+function help(){
+    echo "Please provide the following parameters"
+}
 
 
-while getopts ":cwe:" opt; do
+    
+function parameter(){ while getopts ":cwe:" opt; do
    case $opt in
-       c) input = "$percentage";;
-       w) input = "$percentage";;
-       e) input = "$email";;
-       \?) help; exit;;
+       c) input1="$cpercentage";;
+       w) input2="$wpercentage";;
+       e) input3="$email";;
+       \?) help;exit 1;;
    esac
-      done
-      
-      if ["$input" = "" ]
+ done
+ 
+ 
+      if [ "$input1" = "" ]
       then 
-        echo "Please provide the following parameters: \n[c] Critical Threshold (percentage)
-            \n[w] Warning Threshold (percentage)
-            \n[e] email address to send the report;"
-            exit
+      echo "Please indicate critical threshold"
+      elif [ "$input2" = "" ]
+      then 
+      echo "Please warning critical threshold"
+       elif [ "$input3" = "" ]
+      then 
+      echo "Please enter email"
+      else 
+      verify
         fi
+}
+
+memorycheck
+parameter
+
